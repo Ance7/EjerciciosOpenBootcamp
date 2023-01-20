82b4243ba7b46d5116cc2104cbd1d353cfdd606f
Ance7 <121410013+Ance7@users.noreply.github.com>
2023-01-20 16:06:23 -0300

    Add files via upload

---
+++ JavaScript/Leccion2/ejercicio1.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Anghelo Alva", 
+    19,
+    false,
+    new Date(2003, 9, 19),
+    {
+    titulo: "Cruzada en jeans",
+    autor: "Thea Beckman",
+    fecha: new Date(2008, 5, 17),
+    url: "https://www.amazon.com/-/es/Thea-Beckman/dp/8491074511",
+    }, 
+];
+
+console.log(lista);
