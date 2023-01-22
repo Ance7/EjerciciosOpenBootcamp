1d50c0d1b1195fc91265b236a75cd791e60aec87
Ance7 <121410013+Ance7@users.noreply.github.com>
2023-01-22 17:43:07 -0300

    Add files via upload

---
+++ JavaScript/Leccion5/ejercicio1.js
@@ -0,0 +1,13 @@
+let alturaCM = 169
+
+let alturaM = 1.69
+
+let peso = 54.5
+
+let altura = Math.ceil(alturaM)
+
+let pesoR = Math.floor(peso)
+
+let num = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(num)
